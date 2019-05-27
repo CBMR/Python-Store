16f71494db2520d4eaaaac252dd957bf0da0becd
Abdiel Fernandez <fernandezabdiel@gmail.com>
2019-05-27 18:05:05 -0400

    finished the store class

--- store.py
+++ store.py
@@ -1,3 +1,5 @@
+from categories import Category
+
 class Store:
 
     def __init__(self, name, categories):
@@ -10,10 +12,28 @@ class Store:
         output += f"{self.name} \n"
         i = 1
         for c in self.categories:
-            output += f"{i}. {c} \n"
+            output += f"{i}. {c.name} \n"
             i += 1
+        output += f"{len(self.categories) + 1}. Exit"
         return output
 
 
-myStore = Store("The Dugout", ["Runnig", "Baseball", "Basketball"])
+
+myStore = Store("The Dugout", [Category("Running"), Category("Baseball"), Category("Basketball")])
 print(myStore)
+
+selection = 0
+num_of_categories = len(myStore.categories) + 1
+
+while selection != num_of_categories :
+    selection = input("please select your deparment ")
+    try:
+        selection = int(selection)
+        if selection == num_of_categories:
+            print("Thank you, come back soon")
+        elif selection > 0 and selection <= num_of_categories :
+            print(f"user selected {myStore.categories[selection - 1].name}")
+        else: 
+            print("not a valid selection")
+    except:
+        print("Your selection must be a number")
