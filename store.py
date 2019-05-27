8530492e245f9e6992824679130af51e6e6e2c28
Abdiel Fernandez <fernandezabdiel@gmail.com>
2019-05-27 12:11:19 -0400

    created the Store class

--- store.py
+++ store.py
@@ -1,3 +1,19 @@
 class Store:
 
-    def __init__(self, ):
+    def __init__(self, name, categories):
+        
+        self.name = name
+        self.categories = categories
+
+    def __repr__(self):
+        output = ""
+        output += f"{self.name} \n"
+        i = 1
+        for c in self.categories:
+            output += f"{i}. {c} \n"
+            i += 1
+        return output
+
+
+myStore = Store("The Dugout", ["Runnig", "Baseball", "Basketball"])
+print(myStore)
